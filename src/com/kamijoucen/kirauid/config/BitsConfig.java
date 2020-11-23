0ce37dabe0cfd88fa3e800f77fe4829140185891
Kamijoucen <lisicen12138@outlook.com>
2020-11-23 18:13:59 +0800

    配置构造

--- src/com/kamijoucen/kirauid/config/BitsConfig.java
+++ src/com/kamijoucen/kirauid/config/BitsConfig.java
@@ -3,27 +3,46 @@ package com.kamijoucen.kirauid.config;
 import com.kamijoucen.kirauid.domain.BitPart;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BitsConfig {
 
-    private int allBits = 0;
-
+    private Map<BitsProperties, Long> defaultBits = new HashMap<BitsProperties, Long>();
     private List<BitPart> parts = new ArrayList<BitPart>();
 
-    public boolean addPart(BitsProperties prop) {
+    private long bits = 0;
 
+    public BitsConfig() {
+        defaultBits.put(BitsProperties.TIME, 41L);
+        defaultBits.put(BitsProperties.SEQUENCE, 12L);
+    }
+
+    public boolean addPart(BitsProperties prop) {
+        if (prop == BitsProperties.CUSTOM) {
+            return false;
+        }
+        addPart(prop, defaultBits.get(prop));
         return true;
     }
 
-    public void addPart(BitsProperties prop, int length) {
+    public boolean addPart(BitsProperties prop, long length) {
+        if (prop == BitsProperties.CUSTOM) {
+            return false;
+        }
+        parts.add(new BitPart(length, bits, prop));
+        bits += length;
+        return true;
+    }
 
+    public boolean addCustomPart() {
+        return false;
     }
 
     public static BitsConfig defaultConfig() {
         BitsConfig config = new BitsConfig();
         config.addPart(BitsProperties.TIME);
-        config.addPart(BitsProperties.WORKER);
         config.addPart(BitsProperties.SEQUENCE);
         return config;
     }
--- src/com/kamijoucen/kirauid/config/BitsProperties.java
+++ src/com/kamijoucen/kirauid/config/BitsProperties.java
@@ -4,7 +4,6 @@ public enum BitsProperties {
 
     TIME,
     SEQUENCE,
-    WORKER,
     CUSTOM,
     ;
 
